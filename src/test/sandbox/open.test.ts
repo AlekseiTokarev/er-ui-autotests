70a025ef9a25b9b109398d2970914f31b99a06f0
Aleksei Tokarev <atokarev@hylamobile.com>
2020-04-19 21:52:04 -0500

    add ts test

---
+++ src/test/sandbox/open.test.ts
@@ -0,0 +1,21 @@
+import {sum} from './sumts'
+import {chromium} from "playwright";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+
+it('my first ', async () => {
+
+    const browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://jestjs.io/');
+    const title = await page.title();
+    expect(title).toBe("Jest · 🃏 Delightful JavaScript Testing");
+    await page.type('#search_input_react', 'type script');
+    await page.keyboard.press('Enter');
+    await page.screenshot({path: `jestjs.png`});
+    browser.close();
+}, 300000);
--- src/test/sandbox/sum.test.js
+++ src/test/sandbox/sum.test.js
@@ -1,3 +1,5 @@
+import {chromium} from "playwright";
+
 const sum = require('./sum');
 const openSite = require('./base')
 
@@ -9,9 +11,10 @@ test('adds 1 + 2 to equal 3', () => {
 test('open site', () => {
 
     (async () => {
+        const browser = await chromium.launch();
         const page = await openSite();
         await page.screenshot({ path: `example.png` });
-        const b = await page.browser();
-        b.close();
+        // const b = await page.browser();
+        // b.close();
     })();
 })
